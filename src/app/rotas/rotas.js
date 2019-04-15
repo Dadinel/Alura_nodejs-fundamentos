bd762b3f1d12581e44252cb4a5b37643dee5ea3f
Daniel Mendes De Melo Sousa <daniel.mendes@sp01.local>
2019-04-15 12:57:49 -0300

    Criação de db e utilização de DAO

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,6 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', function(req, resp) {
         resp.send(`<html>
@@ -12,20 +15,14 @@ module.exports = (app) => {
     });
     
     app.get('/livros', function(req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node avançado'
-                    }
-                ]
-            }
-        );
+
+        const livroDao = new LivroDao(db);
+
+        livroDao.lista(function(erro, resultados) {
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                { livros: resultados }
+            );
+        });
     });
 }
